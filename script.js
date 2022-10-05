04693b7f4f6cde55730124fdbae5d5ed7c7ec47f
brett shulman <bshulman810@gmail.com>
2022-10-05 10:36:17 -0400

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var curTime = moment();
+$("#currentDay").text(curTime.format("dddd MMMM Do, YYYY"));
+
+
+var calRow = document.querySelectorAll(".row");
+
+generateRow = function () {
+    calRow.length = 36;
+    for()
+}
